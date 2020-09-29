cc7940c63561fcc0ccc0444c90895a92d1b9ab6f
conilastra <cflastra@uc.cl>
2020-09-29 11:07:14 -0300

    update styles

--- src/data-provider.js
+++ src/data-provider.js
@@ -7,6 +7,10 @@ const rawData = [
 		data: 90,
 		hoverColor: '#38e5f5',
 		icon: 'extension',
+		style: {
+			background: 'rgb(93,188,210)',
+			background: 'linear-gradient(74deg, rgba(93,188,210,1) 0%, rgba(56,229,245,1) 100%)'
+		},
 		text:
 			'Compartimos la noción de que los lazos se trabajan, que debe haber una intencionalidad en construir puentes y dimensiones. Aunque el miedo me nubló un poco, en general estoy muy dispuesta a poner de mi parte para avanzar juntos... espero que esto lo demuestre :)'
 	},
@@ -16,6 +20,10 @@ const rawData = [
 		color: '#71c2e0',
 		hoverColor: '#32c5fb',
 		icon: 'explore',
+		style: {
+			background: 'rgb(113,194,224)',
+			background: 'linear-gradient(74deg, rgba(113,194,224,1) 0%, rgba(50,197,251,1) 100%)'
+		},
 		text:
 			'A largo plazo, estoy muy de acuerdo con tu noción de lo que significa querer: el poder "descansar" en la otra persona, fortalecerse mutuamente, acompañarse, apoyarse. Sin embargo, yo necesito también la explosión bioquímica; aunque sea temporal, aunque se vaya a acabar, la quiero mientras dure (ver más detalles en Emocionalidad).'
 	},
@@ -25,6 +33,10 @@ const rawData = [
 		color: '#77aee6',
 		hoverColor: '#389cff',
 		icon: 'emoji_objects',
+		style: {
+			background: 'rgb(119,174,230)',
+			background: 'linear-gradient(74deg, rgba(119,174,230,1) 0%, rgba(56,156,255,1) 100%)'
+		},
 		text:
 			'Aunque valoro profundamente las reflexiones e intentar posicionarme desde una visión crítica frente al mundo, para mí no todo tiene que ser cuestionable. A mí sí me gusta "dejarme llevar", aunque sea por un rato.'
 	},
@@ -34,15 +46,23 @@ const rawData = [
 		color: '#7e99ed',
 		hoverColor: '#4775ff',
 		icon: 'favorite',
+		style: {
+			background: 'rgb(126,153,237)',
+			background: 'linear-gradient(74deg, rgba(126,153,237,1) 0%, rgba(71,117,255,1) 100%)'
+		},
 		text:
 			'En ambos existe la disposición a sentir, a formar lazos profundos y entendernos mutuamente. Entendemos la importancia de abordar las interacciones desde la responsabilidad afectiva y empatía.'
 	},
 	{
 		id: 'Atracción Física',
 		data: 30,
-		color: '#7e99ed',
+		color: '#7e85f4',
 		hoverColor: '#525eff',
 		icon: 'whatshot',
+		style: {
+			background: 'rgb(126,133,244)',
+			background: 'linear-gradient(74deg, rgba(126,133,244,1) 0%, rgba(82,94,255,1) 100%)'
+		},
 		text:
 			'Mi versión ideal del amor tiene 2 componentes: la atracción física y la emocional. Ambas son independientes e igual de importantes; para mí, atracción emocional sin atracción física es amistad. Cuando hablo de "construir en el aire" me refiero precisamente a intentar armar algo en base a una sola dimensión, sin contemplar que hay otra (o quitándole importancia a ella).'
 	}
