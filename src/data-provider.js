8f3dcf13968f1f0b97e5b20320bc8202ab225f32
conilastra <cflastra@uc.cl>
2020-09-29 05:43:09 -0300

    update data

--- src/data-provider.js
+++ src/data-provider.js
@@ -11,22 +11,22 @@ const rawData = [
 			'Compartimos la noción de que los lazos se trabajan, que debe haber una intencionalidad en construir puentes y dimensiones. Aunque el miedo me nubló un poco, en general estoy muy dispuesta a poner de mi parte para avanzar juntos... espero que esto lo demuestre :)'
 	},
 	{
-		id: 'Expectativa',
-		data: 70,
+		id: 'Expectativas',
+		data: 80,
 		color: '#71c2e0',
 		hoverColor: '#32c5fb',
 		icon: 'explore',
 		text:
-			'En lo general, estoy muy de acuerdo con tu noción de lo que significa querer: el poder "descansar" en la otra persona, fortalecerse mutuamente, acompañarse, apoyarse. Sin embargo, yo necesito también la explosión bioquímica; aunque sea temporal, aunque se vaya a acabar, la quiero mientras dure (ver más detalles en Emocionalidad).'
+			'A largo plazo, estoy muy de acuerdo con tu noción de lo que significa querer: el poder "descansar" en la otra persona, fortalecerse mutuamente, acompañarse, apoyarse. Sin embargo, yo necesito también la explosión bioquímica; aunque sea temporal, aunque se vaya a acabar, la quiero mientras dure (ver más detalles en Emocionalidad).'
 	},
 	{
 		id: 'Racionalidad',
-		data: 60,
+		data: 70,
 		color: '#77aee6',
 		hoverColor: '#389cff',
 		icon: 'emoji_objects',
 		text:
-			'Aunque valoro profundamente las reflexiones e intentar posicionarme desde una visión crítica, para mí no todo tiene que ser cuestionable. A mí sí me gusta "dejarme llevar".'
+			'Aunque valoro profundamente las reflexiones e intentar posicionarme desde una visión crítica frente al mundo, para mí no todo tiene que ser cuestionable. A mí sí me gusta "dejarme llevar", aunque sea por un rato.'
 	},
 	{
 		id: 'Emocionalidad',
@@ -35,7 +35,7 @@ const rawData = [
 		hoverColor: '#4775ff',
 		icon: 'favorite',
 		text:
-			'En ambos existe la disposición a sentir, a formar lazos profundos y entendernos mutuamente. A abordar esta interacción desde la responsabilidad afectiva y empatía.'
+			'En ambos existe la disposición a sentir, a formar lazos profundos y entendernos mutuamente. Entendemos la importancia de abordar las interacciones desde la responsabilidad afectiva y empatía.'
 	},
 	{
 		id: 'Atracción Física',
@@ -44,7 +44,7 @@ const rawData = [
 		hoverColor: '#525eff',
 		icon: 'whatshot',
 		text:
-			'Mi versión ideal del amor tiene 2 componentes: la atracción física y la emocional. Ambas son independientes e igual de importantes; para mí, atracción emocional sin atracción física es amistad. Cuando hablo de "construir en el aire" me refiero precisamente a intentar armar algo en base a un solo un eje, sin contemplar –o quitándole importancia al hecho de– que hay otro.'
+			'Mi versión ideal del amor tiene 2 componentes: la atracción física y la emocional. Ambas son independientes e igual de importantes; para mí, atracción emocional sin atracción física es amistad. Cuando hablo de "construir en el aire" me refiero precisamente a intentar armar algo en base a una sola dimensión, sin contemplar que hay otra (o quitándole importancia a ella).'
 	}
 ];
 
